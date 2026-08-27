0eb446858e0f83ef131c11f1c4d185723bc93298
SMR305 <kennethramsey305@yahoo.com>
2026-08-27 19:43:21 -0400

    Removed obsolete testing function

--- wake/wake/views.py
+++ wake/wake/views.py
@@ -35,45 +35,6 @@ def index(request):
     servers = list(Server.objects.values("name", "is_on"))
     return render(request, "index.html", {"servers": servers})
 
-# Obsolete Test Function
-def toggle_button(request):
-    url_name = request.resolver_match.url_name
-
-    if request.method == "POST" and url_name == "other":
-        server, _ = Server.objects.get_or_create(id=2, name="tmp")
-        server.is_on = not server.is_on
-
-        server.save()
-
-        return JsonResponse({
-            "status": "ok",
-            "enabled": server.is_on,
-            "label": "Enabled" if server.is_on else "Disabled"
-        })
-
-    if request.method == "POST" and url_name == "desktop":
-        server, _ = Server.objects.get_or_create(id=1, name="desktop", mac_address=TEST_MAC)
-        server.is_on = not server.is_on
-
-        if not server.is_on and WAKE_UP:
-            wake(TEST_MAC)
-            print("Called wake")
-        
-        server.save()
-
-        return JsonResponse({
-            "status": "ok",
-            "enabled": server.is_on,
-            "label": "Enabled" if server.is_on else "Disabled"
-        })
-
-    return JsonResponse({"error": "Invalid request"}, status=400)
-
-
-# 
-# Need to handle knowing information about the server to be interacted with via the body of the POST request
-# 
-
 # Shared helper function for adding a new server
 def add(request):
     if request.method == "POST":
