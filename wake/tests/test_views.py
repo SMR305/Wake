9927e8018a2cb833d7ff2ec5a3de735098131a23
SMR305 <kennethramsey305@yahoo.com>
2026-09-17 16:14:43 -0400

    Added mock sockets to properly test the views and extended the testing to include some of those views

--- wake/tests/test_views.py
+++ wake/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test import Client
 from wake.models import Server
+import unittest.mock as mock
 
 class ServerTests(TestCase):
     def setUp(self):
@@ -23,12 +24,44 @@ class ServerTests(TestCase):
         with self.assertRaises(Server.DoesNotExist):
             Server.objects.get(name="test-1")
 
-    def testPower(self):
+    def testPowerOff(self):
+        with mock.patch('socket.socket') as mock_socket:
+            mock_socket.return_value.connect.return_value = None
+            mock_socket.return_value.send.return_value = None
+            Server.objects.create(name="test-1", ip_address="0.0.0.0", mac_address="0:0:0:0:0:0", is_on=True)
+
+            response = self.c.post("/power/", {"name": "test-1"})
+            self.assertEqual(response.status_code, 200)
+            item = Server.objects.get(name="test-1")
+            self.assertEqual(item.is_on, None)
+
+    def testPowerOn(self):
+        with mock.patch('socket.socket') as mock_socket:
+            mock_socket.return_value.connect.return_value = None
+            mock_socket.return_value.send.return_value = None
+            Server.objects.create(name="test-1", ip_address="0.0.0.0", mac_address="0:0:0:0:0:0", is_on=False)
+
+            response = self.c.post("/power/", {"name": "test-1"})
+            self.assertEqual(response.status_code, 200)
+            item = Server.objects.get(name="test-1")
+            self.assertEqual(item.is_on, None)
+
+    def testReboot(self):
+        with mock.patch('socket.socket') as mock_socket:
+            mock_socket.return_value.connect.return_value = None
+            mock_socket.return_value.send.return_value = None
+            Server.objects.create(name="test-1", ip_address="0.0.0.0", mac_address="0:0:0:0:0:0", is_on=True)
+
+            response = self.c.post("/power/", {"name": "test-1"})
+            self.assertEqual(response.status_code, 200)
+            item = Server.objects.get(name="test-1")
+            self.assertEqual(item.is_on, None)
+
+    def testPowerFailConnect(self):
         Server.objects.create(name="test-1", ip_address="0.0.0.0", mac_address="0:0:0:0:0:0", is_on=True)
 
         response = self.c.post("/power/", {"name": "test-1"})
-        # Extend this test more
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, 502)
 
 
 
